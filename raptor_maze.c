8cead58426415a9221bc91e80b3eac14705a46b5
Nathan Piel <nthnpiel@gmail.com>
2024-09-18 20:57:56 -0400

    improved

--- raptor_maze.c
+++ raptor_maze.c
@@ -31,33 +31,104 @@
 #define print printf
 #define KERN_INFO
 #define free_mem free
-
 #endif
 
+#define Vec_define(T) typedef struct { T *data; int len; int capacity; } Vec_##T
+
+#define Vec_alloc_full(vec, n)                                      \
+    do {                                                            \
+        (vec)->data = Arena_alloc(&arena, n, sizeof(*(vec)->data)); \
+        (vec)->len = n;                                             \
+        (vec)->capacity = n;                                        \
+    } while (0)
+
+#define Vec_alloc_empty(vec, n)                                     \
+    do {                                                            \
+        (vec)->data = Arena_alloc(&arena, n, sizeof(*(vec)->data)); \
+        (vec)->len = 0;                                             \
+        (vec)->capacity = n;                                        \
+    } while (0)
+
+#define Vec_push(vec, val) (vec)->data[(vec)->len++] = (val)
+    
+#define Vec_push_extend(vec, val)                                       \
+    do {                                                                \
+        if ((vec)->len >= (vec)->capacity)  {                           \
+            if ((vec)->capacity == 0) (vec)->capacity = 2;              \
+            else (vec)->capacity *= 2;                                  \
+            (vec)->data = Arena_realloc(&arena,                         \
+                                        (vec)->data,                    \
+                                        (vec)->capacity,                \
+                                        sizeof(*(vec)->data));          \
+        }                                                               \
+        Vec_push(vec, val);                                             \
+    } while(0)
+
 typedef struct {
-    void **data;
+    int u;
+    int v;
+} Edge;
+
+typedef struct {
+    int data[4];
     int len;
-    int capacity;
-} Arena;
+} Vec4_int;
+
+typedef void* voidptr;
+
+Vec_define(voidptr);
+Vec_define(Edge);
+Vec_define(int);
+Vec_define(Vec_int);
+Vec_define(Vec4_int);
+Vec_define(char);
+
+typedef Vec_char String;
+typedef Vec_Vec4_int Graph;
+typedef Vec_voidptr Arena;
+
+static Arena arena;
+static int maze_width = 10;
+static int maze_height = 10;
+
+#ifdef KERNAL_MODE
+static struct file_operations proc_ops = {
+    .owner = THIS_MODULE,
+    .read = do_maze,
+    .write = get_input,
+};
+#endif
 
 void *Arena_alloc(Arena *arena, int n, int size);
 void *Arena_realloc(Arena *arena, void *p, int n, int size);
 void Arena_init(Arena *arena);
 void Arena_deinit(Arena *arena);
 
-static Arena arena;
-static int maze_width = 9;
-static int maze_height = 9;
+void make_grid(Vec_Edge *, int width, int height);
+void randomly_sort(Vec_Edge *e);
+int find_set(Vec_Vec_int set, int uv);
+void Graph_insert(Graph *g, Edge e);
+bool Graph_get_adj(Vec4_int av, int index);
+void kruskal_maze(Vec_Edge *e, Graph *mst);
+void make_maze_line(String *s, Graph *mst, int width, int height);
+void make_maze_hash(String *s, Graph *mst, int width, int height);
+void print_graph(String *s, Graph g);
+void print_edges(String *s, Vec_Edge e);
+
+#ifdef KERNAL_MODE
+int rand(void);
+int init(void);
+void deinit(void);
+ssize_t do_maze(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
+ssize_t get_input(struct file *file, const char __user *usr_buf, size_t count, loff_t *pos);
+#endif
 
 void *Arena_alloc(Arena *arena, int n, int size) {
     void *p;
     if (arena->len >= arena->capacity) {
         if (arena->capacity == 0) arena->capacity = 2;
         else arena->capacity *= 2;
-        arena->data = reallocate(arena->data,
-                                 arena->capacity,
-                                 sizeof(void*),
-                                 GFP_KERNEL);
+        arena->data = reallocate(arena->data, arena->capacity, sizeof(void*), GFP_KERNEL);
     }
     p = allocate_zero(n, size, GFP_KERNEL);
     arena->data[arena->len++] = p;
@@ -66,12 +137,14 @@ void *Arena_alloc(Arena *arena, int n, int size) {
 }
 
 void *Arena_realloc(Arena *arena, void *p, int n, int size) {
-    int i;
-    for (i = 0; i < arena->len; ++i) {
-        if (arena->data[i] == p) {
-            void *new_p = reallocate(p, n, size, GFP_KERNEL);
-            arena->data[i] = new_p;
-            return new_p;
+    if (p) {
+        int i;
+        for (i = 0; i < arena->len; ++i) {
+            if (arena->data[i] == p) {
+                void *new_p = reallocate(p, n, size, GFP_KERNEL);
+                arena->data[i] = new_p;
+                return new_p;
+            }
         }
     }
 
@@ -89,106 +162,9 @@ void Arena_deinit(Arena *arena) {
     for (i = 0; i < arena->len; ++i) {
         free_mem(arena->data[i]);
     }
-
+    
     free_mem(arena->data);
 }
-
-#define Vec_define(T) typedef struct { T *data; int len; int capacity; } Vec_##T
-
-#define Vec_push(vec, val)                                              \
-    do {                                                                \
-        if ((vec)->len >= (vec)->capacity)  {                           \
-            if ((vec)->capacity == 0) (vec)->capacity = 2;              \
-            else (vec)->capacity *= 2;                                  \
-                                                                        \
-            (vec)->data = Arena_realloc(&arena,                         \
-                                        (vec)->data,                    \
-                                        (vec)->capacity,                \
-                                        sizeof(*(vec)->data));          \
-        }                                                               \
-        (vec)->data[(vec)->len++] = (val);                              \
-    } while(0)
-
-#define Vec_pop(vec)                            \
-    do {                                        \
-        if ((vec)->len > 0) (--(vec)->len);     \
-    } while (0)                                
-
-#define Vec_contains(set, x, eq, contains)      \
-    do {                                        \
-        int i;                                  \
-        bool broken = false;                    \
-        for (i = 0; i < (set)->len; ++i) {      \
-            if (eq((set)->data[i], (x))) {      \
-                *(contains) = true;             \
-                broken = true;                  \
-                break;                          \
-            }                                   \
-        }                                       \
-        if (!broken) *(contains) = false;       \
-    } while (0)
-
-#define Vec_push_new(set, x, eq)                                \
-    do {                                                        \
-        bool push = true;                                       \
-        int i;                                                  \
-        for (i = 0; i < (set)->len; ++i) {                      \
-            if (eq((set)->data[i], (x))) { push=false; break; } \
-        }                                                       \
-        if (push) Vec_push((set), (x));                         \
-    } while (0)
-
-#define eq(a, b) (a) == (b)
-
-typedef struct {
-    int index;
-} AdjVertex;
-
-#define AdjVertex_eq(a, b) eq((a).index, (b).index)
-
-typedef struct {
-    int u;
-    int v;
-} Edge;
-
-Vec_define(Edge);
-Vec_define(int);
-Vec_define(Vec_int);
-Vec_define(AdjVertex);
-Vec_define(Vec_AdjVertex);
-Vec_define(char);
-
-typedef Vec_char String;
-typedef Vec_Vec_AdjVertex Graph;
-
-void make_grid(Vec_Edge *, int width, int height);
-void randomly_sort(Vec_Edge *e);
-int find_set(Vec_Vec_int set, int uv);
-void Graph_insert(Graph *g, Edge e);
-bool Graph_get_adj(Vec_AdjVertex av, int index);
-void kruskal_maze(Vec_Edge *e, Graph *mst);
-void make_maze(String *s, Graph *mst, int width, int height, bool graph_mode);
-void print_graph(String *s, Graph g);
-void print_edges(String *s, Vec_Edge e);
-
-#ifdef KERNAL_MODE
-int rand(void);
-#endif
-
-#ifdef KERNAL_MODE
-int init(void);
-void deinit(void);
-ssize_t do_maze(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
-#ifdef KERNAL_MODE
-    ssize_t get_input(struct file *file, const char __user *usr_buf, size_t count, loff_t *pos);
-#endif
-
-static struct file_operations proc_ops = {
-    .owner = THIS_MODULE,
-    .read = do_maze,
-    .write = get_input,
-};
-#endif
 
 #ifdef KERNAL_MODE
 int rand(void) {
@@ -203,6 +179,8 @@ void make_grid(Vec_Edge *e, int width, int height) {
     int row = 0;
     int col = 0;
     int i;
+
+    Vec_alloc_empty(e, width*(height-1) + (width-1)*height);
     for (i = 0; i < width*height; ++i) {
         row = i / width;
         col = i % width;
@@ -232,168 +210,96 @@ int find_set(Vec_Vec_int set, int uv) {
     int i;
     for (i = 0; i < set.len; ++i) {
         Vec_int *inner = &set.data[i];
-        bool contains;
-        Vec_contains(inner, uv, eq, &contains);
-        if (contains) return i;
+        int j;
+        for (j = 0; j < inner->len; ++j) {
+            if (inner->data[j] == uv) return i;                
+        }
     }
 
     return 0; 
 }
 
 void Graph_insert(Graph *g, Edge e) {
-    Vec_AdjVertex *eu = &g->data[e.u];
-    Vec_AdjVertex *ev = &g->data[e.v];
-
-    AdjVertex av;
-    av.index=e.v; Vec_push_new(eu, av, AdjVertex_eq);
-    av.index=e.u; Vec_push_new(ev, av, AdjVertex_eq);
+    Vec_push(&g->data[e.u], e.v);
+    Vec_push(&g->data[e.v], e.u);
 }
 
-bool Graph_get_adj(Vec_AdjVertex av, int index) {
+bool Graph_get_adj(Vec4_int av, int index) {
     int i;
-    for (i = 0; i < av.len; ++i) {
-        if (av.data[i].index == index) return true;
+    for (i = 0; i < 4; ++i) {
+        if (av.data[i] == index) return true;
     }
     
     return false;
 }
 
 void kruskal_maze(Vec_Edge *e, Graph *mst) {
-    Vec_Vec_int vertex_sets;
+    Vec_Vec_int vertex_sets = {0};
     int i;
 
     randomly_sort(e);
 
-    vertex_sets.data = Arena_alloc(&arena, mst->len, sizeof(Vec_int));
-    vertex_sets.len = mst->len;
-    vertex_sets.capacity=mst->len;
+    Vec_alloc_full(mst, maze_width*maze_height);
+    Vec_alloc_full(&vertex_sets, mst->len);
 
     for (i = 0; i < vertex_sets.len; ++i) {
         Vec_int *set = &vertex_sets.data[i];
+        Vec_alloc_empty(set, mst->len);        
         Vec_push(set, i);
     }
 
     for (i = 0; i < e->len; ++i) {
-        int u_set = find_set(vertex_sets, e->data[i].u);
-        int v_set = find_set(vertex_sets, e->data[i].v);
-        if (u_set != v_set) {
+        int u_set_idx = find_set(vertex_sets, e->data[i].u);
+        int v_set_idx = find_set(vertex_sets, e->data[i].v);
+        if (u_set_idx != v_set_idx) {
             int j;
+            Vec_int *v_set = &vertex_sets.data[v_set_idx];
+            Vec_int *u_set = &vertex_sets.data[u_set_idx];
+            
             Graph_insert(mst, e->data[i]);
             
-            for (j = 0; j < vertex_sets.data[v_set].len; ++j) {
-                Vec_push(&vertex_sets.data[u_set], vertex_sets.data[v_set].data[j]);
+            for (j = 0; j < v_set->len; ++j) {
+                Vec_push(u_set, v_set->data[j]);
             }
-            vertex_sets.data[v_set].len = 0;
+            v_set->len = 0;
         }
     }
 }
 
-void print_graph(String *s, Graph g) {
-    int i, k;
-    k = 'A';
-    for (i = 0; i < g.len; ++i) {
-        Vec_AdjVertex av = g.data[i];
+void make_maze_line(String *s, Graph *mst, int width, int height) {
+    int i;
+
+    Vec_alloc_empty(s, maze_width*2*maze_height);
+    
+    Vec_push(s, ' ');
+    for (i = 0; i < width*2-1; ++i) Vec_push(s, '_');
+    Vec_push(s, '\n');
+    
+    for (i = 0; i < height; ++i) {
         int j;
         
-        Vec_push(s, '['); Vec_push(s, k); Vec_push(s, ']'); Vec_push(s, '-'); Vec_push(s, '>'); Vec_push(s, '['); 
-        for (j = 0; j < av.len; ++j) {
-            AdjVertex v = av.data[j];
-            Vec_push(s, 'A'+v.index); if (j < av.len-1) Vec_push(s, ' ');
-
-        }
-        Vec_push(s, ']');
-        Vec_push(s, '\n');
-        k++;
-    }
-
-    Vec_push(s, '\0');
-}
-
-void print_edges(String *s, Vec_Edge e) {
-    int i;
-    
-    for (i = 0; i < e.len; ++i) {
-        Vec_push(s, 'A'+e.data[i].u);
-        Vec_push(s, 'A'+e.data[i].v);
-        Vec_push(s, ' ');
-    }
-    Vec_push(s, '\n');
-    Vec_push(s, '\0');
-}
-
-void make_maze(String *s, Graph *mst, int width, int height, bool line_mode) {
-    int i;
-
-    if (line_mode) {
-        Vec_push(s, ' ');
-        for (i = 0; i < width*2-1; ++i) Vec_push(s, '_');
-        Vec_push(s, '\n');
-    
-        for (i = 0; i < height; ++i) {
-            int j;
-        
-            Vec_push(s, '|');
-            for (j = 0; j < width; ++j) {
-                int index = i*width+j;
+        Vec_push(s, '|');
+        for (j = 0; j < width; ++j) {
+            int index = i*width+j;
             
-                bool eastern_edge = Graph_get_adj(mst->data[index], index+1);
-                bool southern_edge = Graph_get_adj(mst->data[index], index+width);
-
-                if (southern_edge) Vec_push(s, ' ');
+            bool eastern_edge = Graph_get_adj(mst->data[index], index+1);
+            bool southern_edge = Graph_get_adj(mst->data[index], index+width);
+            
+            if (southern_edge) Vec_push(s, ' ');
+            else Vec_push(s, '_');
+            
+            if (eastern_edge) {
+                bool next_southern = Graph_get_adj(mst->data[index+1], index+1+width);
+                if (next_southern) Vec_push(s, ' ');
                 else Vec_push(s, '_');
-
-                if (eastern_edge) {
-                    bool next_southern = Graph_get_adj(mst->data[index+1], index+1+width);
-                    if (next_southern) Vec_push(s, ' ');
-                    else Vec_push(s, '_');
-                } else {
-                    Vec_push(s, '|');
-                }
+            } else {
+                Vec_push(s, '|');
             }
-
-            Vec_push(s, '\n');
         }
-    } else {
-        Vec_push(s, ' ');
-        for (i = 0; i < width*2-1; ++i) Vec_push(s, '@');
-        Vec_push(s, '\n');
-
-        for (i = 0; i < height; ++i) {
-            int j;
         
-            Vec_push(s, '@');
-            for (j = 0; j < width; ++j) {
-                int index = i*width+j;
-            
-                bool eastern_edge = Graph_get_adj(mst->data[index], index+1);
-
-                Vec_push(s, ' ');
-                if (eastern_edge) {
-                    Vec_push(s, ' ');
-                } else if (j < width-1){
-                    Vec_push(s, '#');
-                }
-            }
-
-            Vec_push(s, '@');
-            Vec_push(s, '\n');
-            Vec_push(s, '@');
-            for (j = 0; j < width; ++j) {
-                int index = i*width+j;
-
-                bool southern_edge = Graph_get_adj(mst->data[index], index+width);
-                if (southern_edge) Vec_push(s, ' ');
-                else Vec_push(s, '#');
-
-                if (j < width-1){
-                    Vec_push(s, '#');
-                }
-            }
-
-            Vec_push(s, '@');
-            Vec_push(s, '\n');
-        }
+        Vec_push(s, '\n');
     }
+
     Vec_push(s, '\0');
 }
 
@@ -403,53 +309,39 @@ ssize_t do_maze(struct file *file, char __user *usr_buf, size_t count, loff_t *p
     int main(void)
 #endif
     {
-    Vec_Edge grid = {0};
-    Graph spanning_tree;
-    String buffer = {0};
-    /* String buffer_two = {0}; */
-    /* String buffer_three = {0}; */
-    #ifdef KERNAL_MODE
-    unsigned long bytes_not_copied;
-    #endif
-    Arena_init(&arena);
-#ifndef KERNAL_MODE
-    scanf("%d%d", &maze_width, &maze_height);
+        Vec_Edge grid = {0};
+        Graph spanning_tree = {0};
+        String buffer = {0};
+#ifdef KERNAL_MODE
+        unsigned long bytes_not_copied;
 #endif
+        Arena_init(&arena);
+/* #ifndef KERNAL_MODE */
+/*         scanf("%d%d", &maze_width, &maze_height); */
+/* #endif */
+        
+#ifndef KERNAL_MODE
+        srand(time(NULL));
+#endif
+        make_grid(&grid, maze_width, maze_height);
+        kruskal_maze(&grid, &spanning_tree);
+        make_maze_line(&buffer, &spanning_tree, maze_width, maze_height);
 
-
-    spanning_tree.data = Arena_alloc(&arena, maze_width*maze_height, sizeof(Vec_AdjVertex));
-
-    spanning_tree.len = maze_width*maze_height;
-    spanning_tree.capacity=maze_width*maze_height;
-
-    #ifndef KERNAL_MODE
-    srand(time(NULL));
-    #endif
+#ifdef KERNAL_MODE
+        bytes_not_copied = copy_to_user(usr_buf, buffer.data, buffer.len);
+        if (bytes_not_copied > 0) {
+            print(KERN_INFO "proc/raptor_maze: UH OH\n");
+        }
+#else
+        printf("%s", buffer.data);
+#endif    
     
-    make_grid(&grid, maze_width, maze_height);
-    kruskal_maze(&grid, &spanning_tree);
-    make_maze(&buffer, &spanning_tree, maze_width, maze_height, true);
+        Arena_deinit(&arena);
 
-    /* print_graph(&buffer_two, spanning_tree); */
-    /* print_edges(&buffer_three, grid); */
-
-    #ifdef KERNAL_MODE
-    bytes_not_copied = copy_to_user(usr_buf, buffer.data, buffer.len);
-    if (bytes_not_copied > 0) {
-        print(KERN_INFO "proc/raptor_maze: UH OH\n");
-    }
-    #else
-    printf("%s", buffer.data);
-    /* printf("%s", buffer_two.data); */
-    /* printf("%s", buffer_three.data); */
-    #endif    
-    
-    Arena_deinit(&arena);
-
-    #ifdef KERNAL_MODE
-    return buffer.len;
-    #else
-    return 0;
+#ifdef KERNAL_MODE
+        return buffer.len;
+#else
+        return 0;
 #endif
 }
 
